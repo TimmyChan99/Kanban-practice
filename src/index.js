df02a6c216f356cd9d301979078263eb4c3d0b14
Fatima Ezzahra <felemenoun@gmail.com>
2022-02-01 11:26:15 +0100

    Update script -> display the header

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './style.css';
 import renderNavBar from './modules/navBar';
 import getAndDisplay from './modules/get_and_display_data'
 
+renderNavBar();
 
 window.addEventListener('load', () => {
     getAndDisplay();
